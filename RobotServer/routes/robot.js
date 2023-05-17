149c869642463a8ca0033857d26f8da9c4f30495
Candaş Koru <candas.koru@gmail.com>
2023-05-17 11:48:17 +0300

    Yeni dinamikler
    Yeni dinamikler eklendi ve sürüş mekaniğinde iyileştirmeler yapıldı

--- RobotServer/routes/robot.js
+++ RobotServer/routes/robot.js
@@ -1,7 +1,11 @@
 const express =require('express');
 const router = express.Router();
 const RobotController =require('../controllers/robotController');
+//const script = require('../views/script');
 
+//eventEmitter.on('headlampMessage', (message) => {
+//    script.handleHeadlampMessage(message);
+//  });
 
 router.get('/forward', RobotController.robot_forward);
 router.get('/forwardstop', RobotController.robot_forward_stop);
@@ -14,5 +18,11 @@ router.get('/turnleftstop',RobotController.robot_turn_left_stop);
 
 router.get('/turnright',RobotController.robot_turn_right);
 router.get('/turnrightstop',RobotController.robot_turn_right_stop);
+
+router.get('/brake',RobotController.robot_brake);
+router.get('/brakestop',RobotController.robot_brake_stop);
+
+router.get('/headlamp',RobotController.robot_headlamp);
+router.get('/camera',RobotController.robot_camera);
 
 module.exports = router;
